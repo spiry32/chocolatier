6399fc0227d45727bcd3661845fa5f1284443c9b
Alex <103727459+spiry32@users.noreply.github.com>
2023-05-21 18:37:34 +0300

    Update chocolatier-run.bat

--- chocolatier-run.bat
+++ chocolatier-run.bat
@@ -17,8 +17,18 @@ goto exit
 
 :install
     echo Which application would you like to install?
+    echo ------------------Browsers------------------
     echo [1] Vivaldi
-    echo [2] Vim
+    echo [2] Brave Browser
+    echo [3] Opera
+    echo [4] OperaGX
+    echo [5] Google Chrome
+    echo [6] Firefox
+    echo [7] Tor Browser
+    echo -----------------Messaging-----------------
+    echo [8] Discord
+    echo [9] Skype
+    echo [10] Zoom
     set /p installOption=Enter your option:
     if "%installOption%"=="1" (
         echo Installing Vivaldi...
@@ -26,24 +36,71 @@ goto exit
         goto exit
     )
     if "%installOption%"=="2" (
-        echo Installing Vim...
-        scoop install vim
+        echo Installing Brave...
+        scoop install extras/brave
         goto exit
     ) 
+    if "%installOption%"=="3" (
+        echo Installing Opera...
+        scoop install extras/opera
+        goto exit
+    )
+    if "%installOption%"=="4" (
+        echo Installing OperaGX...
+        scoop install extras/opera-gx
+        goto exit
+    )
+    if "%installOption%"=="5" (
+        echo Installing Google Chrome...
+        scoop install extras/googlechrome
+        goto exit
+    )
+    if "%installOption%"=="6" (
+        echo Installing Firefox...
+        scoop install extras/firefox
+        goto exit
+    )
+    if "%installOption%"=="7" (
+        echo Installing Tor Browser...
+        scoop install main/tor
+        goto exit
+    )
+    if "%installOption%"=="8" (
+        echo Installing Discord...
+        scoop install extras/discord
+        goto exit
+    )
+    if "%installOption%"=="9" (
+        echo Installing Skype...
+        scoop install extras/skype
+        goto exit
+    )
+    if "%installOption%"=="10" (
+        echo Installing Tor Zoom...
+        scoop install extras/zoom
+        goto exit
+    )
+
     echo Invalid option. Please select a valid option.
     goto exit
 
 :uninstall
     echo Which application would you like to uninstall?
-    echo [1] Vivaldi
-    echo [2] Vim
+    echo [1] Microsoft Edge
+    echo [2] Vivaldi
+    echo [3] Vim
     set /p uninstallOption=Enter your option:
     if "%uninstallOption%"=="1" (
+        echo Uninstalling Edge...
+        :remove_microsoft_edge
+        goto exit
+    )
+    if "%uninstallOption%"=="2" (
         echo Uninstalling Vivaldi...
         scoop uninstall vivaldi
         goto exit
     )
-    if "%uninstallOption%"=="2" (
+    if "%uninstallOption%"=="3" (
         echo Uninstalling Vim...
         scoop uninstall vim
         goto exit
@@ -63,4 +120,5 @@ goto exit
     goto exit
 
 :exit
+    
 
